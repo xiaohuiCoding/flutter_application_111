5e8e9c45d03d8efe564e7510d76edea12bbd2b6c
xiaohuiCoding <3060258912@qq.com>
2022-11-16 10:47:11 +0800

    async await

--- lib/first.dart
+++ lib/first.dart
@@ -15,28 +15,28 @@ class FirstPage extends StatelessWidget {
               onPressed: () {
                 pushEventOne(context); // 带参数的函数调用方式一
               },
-              child: const Text('push1',
+              child: const Text('事件1',
                   style: TextStyle(color: Colors.white, fontSize: 18))),
           TextButton(
               onPressed: () => pushEventTwo(context), // 带参数的函数调用方式二
-              child: const Text('push2',
+              child: const Text('事件2',
+                  style: TextStyle(color: Colors.white, fontSize: 18))),
+          TextButton(
+              onPressed: () => pushEventThree(context), // 带参数的函数调用方式二
+              child: const Text('事件3',
                   style: TextStyle(color: Colors.white, fontSize: 18))),
         ],
       ),
-      // body: Center(
-      //     child: Image.asset(
-      //   'images/2.0x/fish.png',
-      //   width: 300,
-      //   height: 300,
-      //   fit: BoxFit.cover,
-      // )),
+      // 图片加载资源的几种方式：1.本地资源(需要在pubspec.yaml文件中显式的引用资源) 2.远程资源
       body: Center(
           child: Image.asset(
+        // 'images/2.0x/fish.png',
         'images/dashatars.webp',
         width: 300,
         height: 300,
         fit: BoxFit.cover,
       )),
+
       // body: Center(
       //     child: Image.network(
       //   'http://g.hiphotos.baidu.com/image/pic/item/472309f790529822c4ac8ad0d5ca7bcb0a46d402.jpg',
@@ -53,8 +53,39 @@ class FirstPage extends StatelessWidget {
   }
 
   void pushEventTwo(context) {
-    log('按钮被点击了');
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => const DetailPage()));
+  }
+
+  void pushEventThree(context) {
+    log(logEvent());
+
+    renderSome();
+  }
+
+  // 1.带返回值的常规函数
+  String logEvent() {
+    String str = 'hello world';
+    return str;
+  }
+
+  // 2.执行异步操作的函数
+  renderSome() {
+    doSomeThing().then((value) {
+      log(value);
+    });
+  }
+
+  // 得到"Oh!"后，将"Oh!"修改为"Oh!my god!"
+  doSomeThing() async {
+    String str = await request();
+    str = 'Oh!my god!';
+    return str;
+  }
+
+  // 模拟等待两秒，返回"Oh"!
+  request() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return 'Oh!';
   }
 }
